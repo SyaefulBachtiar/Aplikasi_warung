102b2135c81fd18f315be945217f57ab733a9250
SyaefulBachtiar <syaefulbachtiar2@gmail.com>
2025-05-16 14:11:27 +0700

    ganti dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\BarangController;
 use App\Http\Controllers\TransaksiController;
 
 // Read dan home
-Route::get('/dashboard', [BarangController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('dashboard', [BarangController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 // Create barang
 Route::post('/create', [BarangController::class, 'store'])->name('create.store');
@@ -43,10 +48,6 @@ Route::get('/grafik', [TransaksiController::class, 'grafik'])->name('grafik');
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
